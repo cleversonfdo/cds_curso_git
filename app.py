31934c180afa19546890f91789a5743a56525949
“cleversonfdo” <cleversonfdoliveira@gmail.com>
2023-01-23 20:35:51 -0300

    Criado arquivo de carregamento de dados e ajusado arquivo principal para utilizar esse novo arquivo criado

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
 
-def load_data():
-	return pd.read_csv('data/processed/bikes_completed.csv')
+import streamlit as st
+from src.extraction import load_data
+
+st.set_page_config(layout = 'wide')
 
 def main():
-	df = load_data()
+	df_raw = load_data()
 
-	st.datframe(df)
+	st.datframe(df_raw)
 	
 if __name__ == '__main__':
 	main()
